5e58c3a4327f0cd7b521c1049116d13f6b52a353
narciscisquellacarbonell <narcis.cisquella@students.salle.url.edu>
2021-11-04 20:14:42 +0100

    d

---
+++ src/exemples.js
@@ -0,0 +1,29 @@
+export default [
+    {
+      id: 1,
+      foto:"1",
+      title:"Web Projects I",
+      deadline:"2-2-2",
+      category:"",
+      description:"",
+      completed:""
+    },
+    {
+        id: 2,
+        foto:"2",
+        title:"Linux Kernel Module",
+        deadline:"2-2-2",
+        category:"",
+        description:"",
+        completed:""
+    },
+    {
+        id: 3,
+        foto:"1",
+        title:"Tuition Module",
+        deadline:"2-2-2",
+        category:"",
+        description:"",
+        completed:""
+    }
+  ];
