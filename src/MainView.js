47459e6ea9a3128ad8ca46a50d874f51c4796a38
nar699 <71522083+nar699@users.noreply.github.com>
2021-11-07 22:09:49 +0100

    to

--- src/MainView.js
+++
@@ -1,9 +0,0 @@
-import exemples from "./exemples";
-
-
-
-
-// On load, perform initial render..
-namesList.render(AppState.get(), "user-list-container");
-userForm.render("add-user-container");
-userCount.render(AppState.get(), "user-count-container");
