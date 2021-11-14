b27fc610a2591a249f5e0d4e6fef4ad3daebe912
nar699 <71522083+nar699@users.noreply.github.com>
2021-11-14 13:39:01 +0100

    mi tio2

--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -1,12 +1,19 @@
 import{IndexView} from "../components/IndexView.js";
+//import{Index2View} from "../components/IndexView.js";
 import {Store} from "../components/Store.js";
+
 
 //import {List} from "../components/List(nidea).js";
 
 //let store = new Store();
 document.addEventListener("DOMContentLoaded",()=>{
-    
+    //part dreta
     let store = new Store();
     let indexView = new IndexView(document.getElementById("container"), store)
     indexView.render();
+    //part esq
+    let index2View = new Index2View(document.getElementById("container2"), store)
+    index2View.render();
+
+
 });
